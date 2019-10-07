c0467a1e7d708f9a09c7074f838ceedb109b4dd0
James Schumann <jschumann@gmail.com>
2019-10-07 10:21:52 -0400

    add functions to copy raw extract blobs if conditions are met

---
+++ view-gclassroom-copy/index.ts
@@ -0,0 +1,73 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { createLogObject } from "../SharedCode/createLogObject";
+import { createLogBlob } from "../SharedCode/createLogBlob";
+import { createCallbackMessage } from "../SharedCode/createCallbackMessage";
+import { createEvent } from "../SharedCode/createEvent";
+
+const viewGClassroomCopy: AzureFunction = async function (context: Context, triggerMessage: string): Promise<void> {
+    const functionInvocationID = context.executionContext.invocationId;
+    const functionInvocationTime = new Date();
+    const functionInvocationTimestamp = functionInvocationTime.toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    const functionName = context.executionContext.functionName;
+    const functionEventType = 'WRDSB.Panama.View.GClassroom.Copy';
+    const functionEventID = `panama-functions-${functionName}-${functionInvocationID}`;
+    const functionLogID = `${functionInvocationTime.getTime()}-${functionInvocationID}`;
+
+    const logStorageAccount = process.env['storageAccount'];
+    const logStorageKey = process.env['storageKey'];
+    const logStorageContainer = 'function-view-gclassroom-copy-logs';
+
+    const eventLabel = '';
+    const eventTags = [
+        "panama", 
+    ];
+    let logPayload;
+    let statusCode;
+    let statusMessage;
+
+    const incomingBlob = context.bindings.incomingBlob;
+
+    if (incomingBlob.lenth < 500) {
+        statusCode = "404"
+        statusMessage = "Error: Too few sourece records. Aborting.";
+    } else {
+        statusCode = "200";
+        statusMessage = "Success: Copied trillium-view-gclassroom/incoming.json to trillium-view-gclassroom/now.json.";
+
+        // Copy blob contents
+        context.bindings.nowBlob = incomingBlob;
+    }
+
+    logPayload = {
+        status: statusCode,
+        message: statusMessage,
+        incomingBlob: "trillium-view-gclassroom/incoming.json",
+        outgoingBlob: "trillium-view-gclassroom/now.json"
+    };
+
+    // Log function invocation and results
+    const logObject = await createLogObject(functionInvocationID, functionInvocationTime, functionName, logPayload);
+    const logBlob = await createLogBlob(logStorageAccount, logStorageKey, logStorageContainer, logObject);
+    context.log(logBlob);
+
+    // Callback for internal consumption
+    const callbackMessage = await createCallbackMessage(logObject, statusCode);
+    context.bindings.callbackMessage = JSON.stringify(callbackMessage);
+    context.log(callbackMessage);
+    
+    // Fire event for external consumption
+    const invocationEvent = await createEvent(functionInvocationID, functionInvocationTime, functionInvocationTimestamp, functionName, functionEventType, functionEventID, functionLogID, logStorageAccount, logStorageContainer, eventLabel, eventTags);
+    context.bindings.flynnEvent = JSON.stringify(invocationEvent);
+    context.log(invocationEvent);
+
+    if (statusCode < 400) {
+        // Context done success message
+        context.done(null, logBlob);
+    } else {
+        // Context done error
+        context.done(logBlob);
+    }
+};
+
+export default viewGClassroomCopy;
